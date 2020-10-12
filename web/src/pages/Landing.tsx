22651d98efe42db55e39a2ff630ad2595937864d
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-10-12 14:48:59 -0300

    Add Routes to the application

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import logoImg from '../assets/icons/complete-logo.svg';
+
+import '../styles/global.css';
+import '../styles/pages/landing.css';
+
+const Landing: React.FC = () => (
+  <div id="page-landing">
+    <div className="content-wrapper">
+      <img src={logoImg} alt="Happy Logo" />
+
+      <main>
+        <h1>Leve felicidade para o mundo</h1>
+        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+      </main>
+
+      <div className="location">
+        <strong>Guarapuava</strong>
+        <span>Paraná</span>
+      </div>
+
+      <a href="" className="enter-app">
+        <FiArrowRight size={26} color="rgba(0,0,0, 0.6)" />
+      </a>
+    </div>
+  </div>
+);
+
+export default Landing;
