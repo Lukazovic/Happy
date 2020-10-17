9164e45feaa9fb0ca8b9dfa715b8f4e9b8259acd
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-10-17 11:41:49 -0300

    Add new pages to the routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/app" component={OrphanagesMap} />
+        <Route exact path="/orphanages" component={OrphanagesMap} />
+        <Route exact path="/orphanages/create" component={CreateOrphanage} />
+        <Route exact path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
